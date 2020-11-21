8c7823288442f980fa9173ff34dad2c7c0b0305a
David Bauske <david.bauske@voltstorage.com>
2020-11-21 16:30:45 +0100

    Added logging to release.js

--- automation/release.js
+++ automation/release.js
@@ -6,4 +6,7 @@ const packageJson = require('../package.json')
 const moduleJson = JSON.parse(fs.readFileSync(MODULE_JSON_PATH, { encoding: 'utf-8' }))
 moduleJson.version = packageJson.version
 moduleJson.download = `https://github.com/Oromis/foundry-combat-focus/releases/download/v${packageJson.version}/foundry-combat-focus-v${packageJson.version}.zip`
+
+console.log(`Setting module version to ${moduleJson.version}, download URL to ${moduleJson.download}`)
+
 fs.writeFileSync(MODULE_JSON_PATH, JSON.stringify(moduleJson, null, 2), { encoding: 'utf-8' })
