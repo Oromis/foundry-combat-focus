e2c3e797b2f85223acd85afe06b27f55f66ca161
David Bauske <david.bauske@voltstorage.com>
2020-11-22 21:19:54 +0100

    Added ability to adjust the size ratio between combat tracker and chat

--- foundry-combat-focus.js
+++ foundry-combat-focus.js
@@ -1,7 +1,10 @@
 const MODULE_NAME = 'foundry-combat-focus'
 const CHAT_ON_COMBAT_TRACKER_SETTING = 'showChatOnCombatTrackerTab'
+const COMBAT_TRACKER_TO_CHAT_RATIO_SETTING = 'combatToChatRatio'
 const CHAT_ID = 'chat'
+const COMBAT_ID = 'combat'
 const ACTIVE_CLASS = 'active'
+const STYLE_ID = `${MODULE_NAME}-styles`
 
 function isChatOnCombatTrackerEnabled() {
   return game.settings.get(MODULE_NAME, CHAT_ON_COMBAT_TRACKER_SETTING)
@@ -15,6 +18,33 @@ function getChatElement() {
   return document.getElementById(CHAT_ID)
 }
 
+function createStyleElement() {
+  const style = document.createElement('style')
+  style.setAttribute('id', STYLE_ID)
+  document.head.append(style)
+  return style
+}
+
+function getSizeRatio() {
+  const setting = game.settings.get(MODULE_NAME, COMBAT_TRACKER_TO_CHAT_RATIO_SETTING) || '1:1'
+  const parts = setting.split(':')
+  return {
+    combat: parseFloat(parts[0] || '1'),
+    chat: parseFloat(parts[1] || '1'),
+  }
+}
+
+function updateStyleElement() {
+  const style = document.getElementById(STYLE_ID)
+  const { combat, chat } = getSizeRatio()
+  if (style != null) {
+    style.innerText = `
+      #${CHAT_ID} { flex-grow: ${chat}; }
+      #${COMBAT_ID} { flex-grow: ${combat}; }
+    `
+  }
+}
+
 function updateCombatTrackerStyle() {
   const enable = isChatOnCombatTrackerEnabled()
   if (enable && getActiveTab() === 'combat') {
@@ -22,9 +52,13 @@ function updateCombatTrackerStyle() {
   } else if (getActiveTab() !== CHAT_ID) {
     getChatElement().classList.remove(ACTIVE_CLASS)
   }
+
+  updateStyleElement()
 }
 
 Hooks.on('init', () => {
+  createStyleElement()
+
   game.settings.register(MODULE_NAME, CHAT_ON_COMBAT_TRACKER_SETTING, {
     name: 'Show chat on combat tracker tab',
     hint: 'Displays the chat log below the combat tracker',
@@ -32,6 +66,28 @@ Hooks.on('init', () => {
     config: true,
     type: Boolean,
     default: true,
+    onChange: updateCombatTrackerStyle
+  })
+
+  game.settings.register(MODULE_NAME, COMBAT_TRACKER_TO_CHAT_RATIO_SETTING, {
+    name: 'Combat Tracer to Chat ratio',
+    hint: 'Changes the ratio by which the chat and the combat tracker are divided. Default: 1:1',
+    scope: 'client',
+    config: true,
+    type: String,
+    choices: {
+      '1:0': '1:0',
+      '4:1': '4:1',
+      '3:1': '3:1',
+      '5:2': '5:2',
+      '2:1': '2:1',
+      '1:1': '1:1',
+      '1:2': '1:2',
+      '2:5': '2:5',
+      '1:3': '1:3',
+      '1:4': '1:4',
+    },
+    default: '1:1',
     onChange: updateCombatTrackerStyle
   })
 
